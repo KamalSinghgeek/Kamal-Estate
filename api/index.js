3adb7d2e03056f77c45af7996ce61c0009690c04
Kamal Singh <kamalsingh23056@gmail.com>
2023-11-23 22:11:09 +0530

    Add existing project files to Git

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+mongoose.connect("mongodb+srv://kamalsingh23056:KamalPass@mern-app.ekxdazl.mongodb.net/?retryWrites=true&w=majority")
 
 const app=express();
 
 app.listen(3000,() =>{
-console.log('Server is running on port 3000!!');
+console.log('Server is running on port 3000!!!');
 }
 );
