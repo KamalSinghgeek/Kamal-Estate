288356162550710b552cd9b67c0146870eb40f1e
Kamal Singh <kamalsingh23056@gmail.com>
2023-12-01 01:40:38 +0530

    Add

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() =>
+
+
+mongoose.connect(process.env.MONGO).then(()  =>
 {
     console.log('Connected to MongoDB !');
 }).catch((err) =>{
