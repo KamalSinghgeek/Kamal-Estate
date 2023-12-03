86998553be77a912ac240f8960cbcffc96292eea
Kamal Singh <kamalsingh23056@gmail.com>
2023-12-03 21:09:33 +0530

    Create Sign Up API Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import  express from 'express';
-import {test} from '../controllers/user.controller.js';
+import { signup } from '../controllers/auth.controller.js';
 const router= express.Router();
 
-router.get('/test',test);
+router.post('/signup',signup);
 
 export default router;
 
